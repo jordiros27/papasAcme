3dc4290015f4d434a0d45a54c0557261ca5c2c02
jordiros <jorroma2@upv.edu.es>
2022-10-11 19:33:59 +0200

    Corrección error reboot instancia

--- aws/app-acme.sh
+++ aws/app-acme.sh
@@ -1,8 +1,7 @@
-#!/bin /bash
+#!/bin/bash
 
 # Prepara la instacia
 sudo yum update
-sudo yum -y install git
 
 # Instalar terraform
 wget https://releases.hashicorp.com/terraform/0.14.3/terraform_0.14.3_linux_amd64.zip
@@ -18,4 +17,7 @@ sudo usermod -aG docker ec2-user
 
 # Preparar script para reboot
 cd
-sh /terraform/local/startup.sh
+cp /terraform/local/startup.sh /etc/init.d
+
+# Reinicio de la instancia
+sudo reboot
--- local/startup.sh
+++ local/startup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # Ejecutar terraform
-mkdir terraform/
-cd terraform/
-git init
-git pull https://github.com/jordiros27/papasAcme.git
-cd local/
+cd terraform/local/
 mkdir execution/
-cp main.tf execution/
-cd execution/
 terraform init 
 terraform apply -auto-approve
